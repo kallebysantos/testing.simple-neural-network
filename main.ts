40ac94ed493dd1adc182d7507d7ecf7382a8d647
kallebysantos <kalleby_santos@hotmail.com>
2024-12-01 20:12:16 +0000

    train: using SGD training

--- main.ts
+++ main.ts
@@ -1,26 +1,35 @@
+import { assertEquals } from "@std/assert";
 import { getDataset } from "./src/dataset/index.ts";
-import { foward } from "./src/neural-network/index.ts";
+import { foward, predict } from "./src/neural-network/index.ts";
 
 if (import.meta.main) {
   const dataset = getDataset();
 
-  let weigth = 1.6;
-  const bias = 32;
+  let weigth = 2.5;
+  let bias = 30;
   const learningRate = 0.002;
 
-  for (let index = 0; index < 20; index++) {
-    const result = foward(
-      weigth,
-      -10,
-      14,
-      learningRate,
-      bias,
-    );
+  for (let index = 0; index < 1_000; index++) {
+    dataset.forEach((observation) => {
+      const result = foward(
+        weigth,
+        observation.celsius,
+        observation.fahrenheit,
+        learningRate,
+        bias,
+      );
 
-    console.log(index, result);
-
-    weigth = result.newWeight;
+      weigth = result.newWeight;
+      bias = result.newBias;
+    });
   }
 
-  //console.log(dataset);
+  console.log(weigth, bias);
+
+  // Test
+  dataset.forEach((observation) => {
+    const result = predict(observation.celsius, weigth, bias);
+
+    assertEquals(observation.fahrenheit, result);
+  });
 }
